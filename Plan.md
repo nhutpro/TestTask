448821b1924a1c802c017aa54f02cc38b0d491f6
Tran Nhut <tnhut803@gmail.com>
2022-06-21 19:51:18 +0700

    create structure code

---
+++ Plan.md
@@ -0,0 +1,3 @@
+Page : I will build three page : Home , List Joke , Detail Joke
+After check API. I just have api call all joke. therefore, i will do fake api for app to get joke
+base on category
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Hi! I'm Tran Nhut</h1>
+    </div>
+  );
+}
+
+// Render component App vào #root element
+ReactDOM.render(<App />, document.getElementById("root"));
